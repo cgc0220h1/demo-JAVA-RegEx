0f7a5b481e7ec849360b31d8b161d57afa387a10
Duc <vanduc2514@gmail.com>
2020-04-15 19:30:32 +0700

    Thêm regex crawl tên bài hát không kèm ca sỹ

--- src/com/crawler/SongNameCrawler.java
+++ src/com/crawler/SongNameCrawler.java
@@ -10,6 +10,8 @@ import java.util.regex.Pattern;
 
 public class SongNameCrawler {
     public static void main(String[] args) {
+        String regexTitleOnly = "'title']\\);\"\\s>(..*?)</a>";
+        String regexTitleAndArtist = "title=\"([^Nghe bài hát]..*?)\"\\sonclick=\"lt\\(\\['genre','list-song'";
         Scanner scanner;
         try {
             URL url = new URL("https://www.nhaccuatui.com/bai-hat/nhac-tre-moi.html");
@@ -17,8 +19,18 @@ public class SongNameCrawler {
             scanner.useDelimiter("\\Z");
             String content = scanner.next();
             scanner.close();
-            Pattern pattern = Pattern.compile("title=\"([^Nghe bài hát]..*?)\"\\sonclick=\"lt\\(\\['genre','list-song'");
+
+            Pattern pattern = Pattern.compile(regexTitleOnly);
             Matcher matcher = pattern.matcher(content);
+            System.out.println("Danh sách bài hát: \n");
+            while (matcher.find()) {
+                System.out.println(matcher.group(1));
+            }
+
+            pattern = Pattern.compile(regexTitleAndArtist);
+            matcher = pattern.matcher(content);
+            System.out.println("--------------------------------------------");
+            System.out.println("Danh sách tên bài hát và ca sĩ: \n");
             while (matcher.find()) {
                 System.out.println(matcher.group(1));
             }
